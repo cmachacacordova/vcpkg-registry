af50564835cb293a2479822d9649bca7d25b896d
Carlos Machaca <cmachaca@technisys.com>
2026-04-25 13:59:34 -0300

    Update i18n-redis to 0.3.1 and update baseline to 2026-04-25

--- ports/i18n-redis/portfile.cmake
+++ ports/i18n-redis/portfile.cmake
@@ -1,9 +1,10 @@
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL "https://github.com/cmachacacordova/i18n-redis.git"
-    REF 66916ee881db958853973c63326ec13bb69a0273 # tag v0.3.0
-    SHA512 7de8b4b6e24f1cff179a890173ff20b0418ae163518226cae21663a0654504496041ae4e01c607ed823ad69132a50f7c049030d51aeaff0804358c44b64c7cce
+    REPO cmachacacordova/i18n-redis
+    REF v0.3.1
+    SHA512 458c5c06cbe47e3342f32e68a26bff07f8d1cb0209c3e6a720ac7e4085750c2ce447612555eff4029941af9a1c106387a937cfb2d852ed60fbb889e0836b5441
 )
+
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/i18n-redis)
