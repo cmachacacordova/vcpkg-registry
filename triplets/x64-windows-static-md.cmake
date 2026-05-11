9a4b7d202a63195a65a5b5446bbdd295388c596d
Carlos Machaca <carloscordova96@hotmail.com>
2026-05-11 16:22:22 -0300

    Optimize triplets configuration

--- triplets/x64-windows-static-md.cmake
+++ triplets/x64-windows-static-md.cmake
@@ -1,5 +1,4 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
+include(${CMAKE_CURRENT_LIST_DIR}/configurations/windows-configuration.cmake)
+
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-
-include(${CMAKE_CURRENT_LIST_DIR}/configurations/windows-configuration.cmake)
--- triplets/x64-windows-static-release.cmake
+++ triplets/x64-windows-static-release.cmake
@@ -1,7 +1,6 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
+include(${CMAKE_CURRENT_LIST_DIR}/configurations/windows-configuration.cmake)
+
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_BUILD_TYPE release)
-
-include(${CMAKE_CURRENT_LIST_DIR}/configurations/windows-configuration.cmake)
