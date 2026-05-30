4029b3e7cbbae9d1a9ff80a3624c9ba79ad2a4fc
Carlos Machaca <carloscordova96@hotmail.com>
2026-05-30 18:20:10 -0300

    Update i18n-redis to 1.0.0-SNAPSHOT with JSON backend feature support and remove update-port scripts

--- ports/i18n-redis/portfile.cmake
+++ ports/i18n-redis/portfile.cmake
@@ -1,11 +1,29 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cmachacacordova/i18n-redis
-    REF v0.3.2
-    SHA512 b00ace7f59d78262afd1b87639c466284659433a43cb823e9011894eaa3f95649b2474f3706c3f5ca55ae930d17c37f63427798a637118e8294f68f2d8b3301a
+    REF 1.0.0-SNAPSHOT
+    SHA512 c50b7798f0965efe3a1575b5acb15a15d284033bf0e1b80f273654e6f10a0d96b60010c8319986624f7d545aae3400a1dc0d012d64571f61181da016beaa0e5c
 )
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        simdjson I18N_REDIS_USE_SIMDJSON
+        yyjson   I18N_REDIS_USE_YYJSON
+)
+
+if("simdjson" IN_LIST FEATURES)
+    set(I18N_REDIS_JSON_BACKEND "simdjson")
+elseif("yyjson" IN_LIST FEATURES)
+    set(I18N_REDIS_JSON_BACKEND "yyjson")
+else()
+    set(I18N_REDIS_JSON_BACKEND "simdjson")
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DI18N_REDIS_JSON_BACKEND=${I18N_REDIS_JSON_BACKEND}
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/i18n-redis)
 file(INSTALL ${SOURCE_PATH}/README.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
