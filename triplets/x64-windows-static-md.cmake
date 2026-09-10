de86cb5005d9ee7cfff1e10737be442b206005b6
Carlos Machaca <cmachaca@technisys.com>
2026-09-09 21:56:02 -0300

    Remove custom vcpkg triplet configurations

--- triplets/x64-windows-static-md.cmake
+++
@@ -1,4 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/configurations/windows-configuration.cmake)
-
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
--- triplets/x64-windows-static-release.cmake
+++
@@ -1,6 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/configurations/windows-configuration.cmake)
-
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE static)
-
-set(VCPKG_BUILD_TYPE release)
